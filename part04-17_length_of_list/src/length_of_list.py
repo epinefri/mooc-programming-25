30d5c03e1637c3afcd37b1c1124d6bebab2122be
epinefri <danielasorinap@gmail.com>
2025-09-10 10:09:36 +0300

    routine backup

--- part04-17_length_of_list/src/length_of_list.py
+++ part04-17_length_of_list/src/length_of_list.py
@@ -1,4 +1,7 @@
 # Write your solution here
+def length(input_list):
+    return len(input_list)
+
 # You can test your function by calling it within the following block
 if __name__ == "__main__":
     my_list = [3, 6, -4]
--- part04-18_mean/src/mean.py
+++ part04-18_mean/src/mean.py
@@ -1,4 +1,6 @@
 # Write your solution here
+def mean(input_list):
+    return sum(input_list)/len(input_list)
 # You can test your function by calling it within the following block
 if __name__ == "__main__":
     my_list = [3, 6, -4]
