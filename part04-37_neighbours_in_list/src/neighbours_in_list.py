9bf84b278b7b5f1187ec8666153dfb60447c80d6
epinefri <danielasorinap@gmail.com>
2025-09-14 17:42:49 +0300

    one and a half

--- part04-37_neighbours_in_list/src/neighbours_in_list.py
+++ part04-37_neighbours_in_list/src/neighbours_in_list.py
@@ -1,19 +1,25 @@
 # Write your solution here
 
 def longest_series_of_neighbours(list_of_numbers):
-    neighbours = []
-    count_neighbours = 0
-    for element in list_of_numbers:
-        for next_element in list_of_numbers[1:]:
-            if next_element - element == 1:
-                count_neighbours += 1
-                print(element, "->", next_element, "#" , count_neighbours)
-            else:
-                count_
-    return count_neighbours
+    previous = list_of_numbers[0]
+    count = 1
+    list_counts = []
+    for element in list_of_numbers[1:]:
+        if element - previous in (-1, 1):
+            count += 1
+        else:
+            count = 1
+        if count not in list_counts:
+            list_counts.append(count)
+        previous = element
+    if list_counts == []:
+        return 1
+    else:
+        return(max(list_counts))
+     
 
 
 
 if __name__ == "__main__":
-    my_list = [1, 2, 5, 7, 6]#, 5, 6, 3, 4, 1, 0]
+    my_list = [1, 3, 5, 7, 10, 11, 14, 15, 19, 20, 21, 22, 23, 24, 25]
     print(longest_series_of_neighbours(my_list))
--- part04-38_grade_statistics/src/grade_statistics.py
+++ part04-38_grade_statistics/src/grade_statistics.py
@@ -1,1 +1,17 @@
-# Write your solution here
+# Write your solution 
+
+def get_input():
+    while True:
+        points_exercises = input("Exam points and exercises completed: ")
+        if points_exercises == '':
+            break
+        split_list = points_exercises.split()
+        points = int(split_list[0])
+        exercises = int(split_list[1])
+        print(points, exercises)
+
+
+def main():
+    get_input()
+
+main()
