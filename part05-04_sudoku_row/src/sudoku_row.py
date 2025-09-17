a16667cf9d34f4113b90c891630d30bc065c8e1e
epinefri <danielasorinap@gmail.com>
2025-09-17 15:20:47 +0300

    4 ex

---
+++ part05-04_sudoku_row/src/sudoku_row.py
@@ -0,0 +1,33 @@
+# Write your solution here
+
+def row_correct(sudoku: list, row_no: int):
+
+    chosen_row = []
+    # keep only positive values
+    for element in sudoku[row_no]:
+        if element > 0:
+            chosen_row.append(element)
+
+    # count to find duplicates
+    count = 0
+    for element in chosen_row:
+        count = chosen_row.count(element)
+        if count == 2:
+            return False
+    return True
+
+if __name__ == "__main__":
+    sudoku = [
+  [9, 0, 0, 0, 8, 0, 3, 0, 0],
+  [2, 0, 0, 2, 5, 0, 7, 0, 0],
+  [0, 2, 0, 3, 0, 0, 0, 0, 4],
+  [2, 9, 4, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 7, 3, 0, 5, 6, 0],
+  [7, 0, 5, 0, 6, 0, 4, 0, 0],
+  [0, 0, 7, 8, 0, 3, 9, 0, 0],
+  [0, 0, 1, 0, 0, 0, 0, 0, 3],
+  [3, 0, 0, 0, 0, 0, 0, 0, 2]
+    ]
+
+    print(row_correct(sudoku, 0))
+    print(row_correct(sudoku, 1))
