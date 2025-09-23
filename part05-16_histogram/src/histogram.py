a978170175d303bf04074d195bda78db83722c68
epinefri <danielasorinap@gmail.com>
2025-09-23 16:16:47 +0300

    backup

--- part05-16_histogram/src/histogram.py
+++ part05-16_histogram/src/histogram.py
@@ -1,1 +1,10 @@
-# Write your solution here
+def histogram(word):
+    count = {}
+    for letter in word:
+        if letter not in count:
+            count[letter] = 0
+        count[letter] += 1
+
+    for key, value in count.items():
+        print(key, "*" * value)
+
--- part05-17_phone_book_v1/src/phone_book_v1.py
+++ part05-17_phone_book_v1/src/phone_book_v1.py
@@ -1,1 +1,23 @@
 # Write your solution here
+
+phone_book = {"mary": 123, "john": 456}
+while True:
+    command = int(input("command (1 search, 2 add, 3 quit):"))
+    if command == 3:
+        print("quitting...")
+        break
+
+    if command == 1:
+        searched_name = input("name:")
+        if searched_name not in phone_book:
+            print("no number")
+            break
+        number = print("number:", phone_book[searched_name])
+
+    if command == 2:
+        input("name:")
+        input("number:")
+        print("ok!")
+    
+
+
