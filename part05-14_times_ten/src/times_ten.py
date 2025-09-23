3b707c73d5f1ce46bcef4f0a4b027d5d52676805
epinefri <danielasorinap@gmail.com>
2025-09-23 14:59:34 +0300

    backup

--- part05-14_times_ten/src/times_ten.py
+++ part05-14_times_ten/src/times_ten.py
@@ -1,1 +1,9 @@
-# Write your solution here
+def times_ten(start_index: int, end_index: int):
+    dict = {}
+    for i in range(start_index, end_index+1):
+        dict[i]  = 10 * i 
+    return dict
+
+if __name__ == "__main__":
+    d = times_ten(3,6)
+    print(d)
--- part05-15_factorials/src/factorials.py
+++ part05-15_factorials/src/factorials.py
@@ -1,1 +1,14 @@
-# Write your solution here
+
+def factorials(n: int):
+    k = {}
+    fact = 1
+    for i in range(1, n+1):
+        fact *= i
+        k[i] = fact
+
+    return k
+
+
+if __name__ == "__main__":
+    k = factorials(2)
+    print(k)
