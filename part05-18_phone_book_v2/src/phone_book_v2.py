f9d0a09db7c9d55388dafa8f26d0c9efc2eadc4f
epinefri <danielasorinap@gmail.com>
2025-09-24 15:41:43 +0300

    feat: implement phone book functionality with search and add commands

--- part05-18_phone_book_v2/src/phone_book_v2.py
+++ part05-18_phone_book_v2/src/phone_book_v2.py
@@ -1,1 +1,30 @@
 # Write your solution here
+
+phone_book = {}
+command = 0
+
+
+while True:
+
+    if command == 3:
+        print("quitting...")
+        break
+    
+    command = int(input("command (1 search, 2 add, 3 quit):"))
+    
+    if command == 1:
+        name = input("name:")
+        if name not in phone_book:
+            print('no number')
+            continue
+        for number in phone_book[name]:
+            print(number)
+
+    if command == 2:
+        name = input("name:")
+        number_input = input("number:")
+        if name not in phone_book:
+            phone_book[name] = []
+        phone_book[name].append(number_input)
+        print("ok!")
+        continue
