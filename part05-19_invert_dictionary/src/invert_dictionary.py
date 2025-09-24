63046d96df6c3be1aa1c0e626c90e119de19a786
epinefri <danielasorinap@gmail.com>
2025-09-24 15:52:13 +0300

    feat: implement invert dictionary function

--- part05-19_invert_dictionary/src/invert_dictionary.py
+++ part05-19_invert_dictionary/src/invert_dictionary.py
@@ -1,1 +1,16 @@
 # Write your solution here
+def invert(dictionary: dict):
+    i = {}
+
+    for key, value in dictionary.items():
+        i[value] = key
+
+    dictionary.clear()
+    
+    for key, value in i.items():
+        dictionary[key] = value
+
+if __name__ == "__main__":
+    s = {1: "first", 2: "second", 3: "third", 4: "fourth"}
+    invert(s)
+    print(s)
