7495595c2835ac6cd3afadabca320a05258d6a65
epinefri <danielasorinap@gmail.com>
2025-09-24 16:06:59 +0300

    feat: implement function to convert numbers to their spelled-out form

--- part05-20_numbers_spelled_out/src/numbers_spelled_out.py
+++ part05-20_numbers_spelled_out/src/numbers_spelled_out.py
@@ -1,1 +1,24 @@
 # Write your solution here
+def dict_of_numbers():
+    dict = {}
+    first_ten = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
+    teens = ['eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen']
+    tens = ['ten', 'twenty', 'thirty', 'fourty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety']
+
+    for i in range(0,100):
+        if i < 10:
+            dict[i] = first_ten[i]
+        elif i < 100:
+            if i % 10 == 0:
+                dict[i] = tens[int(i/10)-1]
+            
+    return dict
+
+if __name__ == "__main__":
+    numbers = dict_of_numbers()
+    """print(numbers[2])
+    print(numbers[11])
+    print(numbers[45])
+    print(numbers[99])
+    print(numbers[0])"""
+    print(numbers)
