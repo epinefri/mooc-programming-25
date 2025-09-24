748a3f7f3e2b3df6131f3cee6913334a8c6b114e
epinefri <danielasorinap@gmail.com>
2025-09-24 15:24:20 +0300

    refactor: initialize phone book as empty and improve command handling

--- part05-17_phone_book_v1/src/phone_book_v1.py
+++ part05-17_phone_book_v1/src/phone_book_v1.py
@@ -1,6 +1,7 @@
 # Write your solution here
 
-phone_book = {"mary": 123, "john": 456}
+phone_book = {}
+
 while True:
     command = int(input("command (1 search, 2 add, 3 quit):"))
     if command == 3:
@@ -11,13 +12,13 @@ while True:
         searched_name = input("name:")
         if searched_name not in phone_book:
             print("no number")
-            break
-        number = print("number:", phone_book[searched_name])
+            continue
+        number = print(phone_book[searched_name])
 
     if command == 2:
-        input("name:")
-        input("number:")
+        name = input("name:")
+        number = input("number:")
+        phone_book[name] = number
         print("ok!")
     
-
 
