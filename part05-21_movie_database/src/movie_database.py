87b109be5be1b339968337ea767b62213790478d
epinefri <danielasorinap@gmail.com>
2025-09-25 14:25:09 +0300

    feat: implement add_movie function to manage movie database

--- part05-21_movie_database/src/movie_database.py
+++ part05-21_movie_database/src/movie_database.py
@@ -1,1 +1,15 @@
-# Write your solution here
+def add_movie(database: list, name: str, director: str, year: int, runtime: int):
+    # database is a list of dictionaries
+    movie_db = {}
+    movie_db["name"] = name
+    movie_db["director"] = director
+    movie_db["year"] = year
+    movie_db["runtime"] = runtime
+
+    database.append(movie_db)
+
+if __name__ == "__main__":
+    database = []
+    add_movie(database, "Gone with the Python", "Victor Pything", 2017, 116)
+    add_movie(database, "Pythons on a Plane", "Renny Pytholin", 2001, 94)
+    print(database)
