30e969d110697a86cdd020314297faf5f5cad2c1
epinefri <danielasorinap@gmail.com>
2025-09-25 14:11:16 +0300

    fix: correct number spelling logic and update list for completeness

--- part05-20_numbers_spelled_out/src/numbers_spelled_out.py
+++ part05-20_numbers_spelled_out/src/numbers_spelled_out.py
@@ -1,24 +1,27 @@
 # Write your solution here
 def dict_of_numbers():
     dict = {}
-    first_ten = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
+    first_ten = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten']
     teens = ['eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen']
-    tens = ['ten', 'twenty', 'thirty', 'fourty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety']
+    tens = ['ten', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety']
 
     for i in range(0,100):
-        if i < 10:
+        if i <= 10:
             dict[i] = first_ten[i]
+        elif i < 20:
+            dict[i] = teens[i - 11]
         elif i < 100:
             if i % 10 == 0:
                 dict[i] = tens[int(i/10)-1]
-            
+            else:
+                dict[i] = tens[int(i/10)-1] + '-' + first_ten[i%10]
     return dict
 
 if __name__ == "__main__":
     numbers = dict_of_numbers()
-    """print(numbers[2])
+    print(numbers[2])
     print(numbers[11])
     print(numbers[45])
     print(numbers[99])
-    print(numbers[0])"""
+    print(numbers[0])
     print(numbers)
