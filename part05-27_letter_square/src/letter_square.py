20706242f8ff3737c48494b637f8e9ed066f6f5f
epinefri <danielasorinap@gmail.com>
2025-09-30 09:59:15 +0300

    feat: implement letter square generation and character listing

--- part05-27_letter_square/src/letter_square.py
+++ part05-27_letter_square/src/letter_square.py
@@ -1,1 +1,15 @@
-# Write your solution here
+layers = 4 # input("Layers:")
+
+# create list of characters
+letters = []
+for i in range(layers):
+    letters.append(chr(i+65))
+
+# print square
+for i in range(layers*2-1): #rows
+    for j in range(layers*2-1):
+        print(' * ',end='')
+    print()
+
+
+print(letters)
