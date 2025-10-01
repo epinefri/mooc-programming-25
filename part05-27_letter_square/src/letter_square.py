d1d4bedfe7d45c9097ed9bc3e8dc869c1cc13eae
epinefri <danielasorinap@gmail.com>
2025-10-01 09:38:42 +0300

    feat: adjust layers for letter square generation and refine print logic

--- part05-27_letter_square/src/letter_square.py
+++ part05-27_letter_square/src/letter_square.py
@@ -1,4 +1,4 @@
-layers = 4 # input("Layers:")
+layers = 3# int(input("Layers:"))
 
 # create list of characters
 letters = []
@@ -6,10 +6,25 @@ for i in range(layers):
     letters.append(chr(i+65))
 
 # print square
-for i in range(layers*2-1): #rows
-    for j in range(layers*2-1):
-        print(' * ',end='')
+for i in range(layers):
+    for j in range(layers-1, -1, -1):
+        print(letters[j],end='')
+    for k in range(1,layers):
+        print(letters[k], end='')
     print()
 
+    
 
-print(letters)
+"""
+CCCCC
+CBBBC
+CBABC
+CBBBC
+CCCCC
+
+22222
+21112
+21012
+
+
+"""
