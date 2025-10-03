b1ccbb46be2471cfd29d8446d3ca5af288e6081c
epinefri <danielasorinap@gmail.com>
2025-10-03 14:12:37 +0300

    feat: implement read_fruits function to load fruit data from CSV

--- part06-02_fruit_market/src/fruit_market.py
+++ part06-02_fruit_market/src/fruit_market.py
@@ -1,1 +1,10 @@
 # write your solution here
+def read_fruits():
+    with open("fruits.csv") as fruit_file:
+        fruits = {}
+        for line in fruit_file:
+            line = line.replace("\n", "")
+            fruit = line.split(';')
+            fruits[fruit[0]] = float(fruit[1])
+    return fruits
+
