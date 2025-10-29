f15f35d66aa2dc98e4c93c18bd6c46047e4d8525
epinefri <danielasorinap@gmail.com>
2025-10-29 15:05:00 +0200

    feat: enhance output formatting for student grading calculations

--- part06-06_course_grading_part_3/src/course_grading_part_3.py
+++ part06-06_course_grading_part_3/src/course_grading_part_3.py
@@ -19,7 +19,7 @@ with open(student_info) as file:
         if parts[0] == 'id':
             continue
         names[parts[0]] = f"{parts[1].strip()} {parts[2].strip()}"
-print(names)
+print("\n names:", names)
 
 # number of exercises from exercise_data files
 exec_nbr = {}
@@ -32,9 +32,26 @@ with open(exercise_data) as file:
         for part in parts[1:]:
             part = int(part.strip())
             exec_nbr[parts[0]] += part
-print(exec_nbr)
+print("\n exer num:", exec_nbr)
 
 # exercises points - to be calculated
+exec_pts = {}
+for student, number_ex in exec_nbr.items():
+    exec_pts[student] = number_ex//4
+print("\n exerc points:", exec_pts)
+"""
+    10% -> 1 point  = 4 exercises
+    20% -> 2 points = 8 exercises
+    30      3         12
+    40      4         16
+    50% -> 5 points = 20 exercises
+    60% -> 6 points = 24
+    70     7          28
+    80     8          32
+    90     9          36
+    100 -> 10 points = 40 exercises
+"""
+
 
 # exam points from exam_points files
 exm_pts = {}
@@ -47,4 +64,15 @@ with open(exam_points) as file:
         for part in parts[1:]:
             part = int(part.strip())
             exm_pts[parts[0]] += part
-print(exm_pts)
+print("\n exam points:", exm_pts)
+
+# total points
+tot_pts = {}
+for student, points in exec_pts.items():
+    tot_pts[student] = points
+for student, points in exm_pts.items():
+    tot_pts[student] += points
+print("\n total:", tot_pts)
+
+
+print("\n")
